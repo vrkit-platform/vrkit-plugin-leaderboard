2e5ce732a2f3b4cce1b7d6e5c9540dfa0a8ca1f4
Jonathan Glanz <jonathan.glanz@gmail.com>
2024-12-24 12:55:38 -0500

    temporarilly stripped shared-ui

--- src/leaderboard/LeaderboardOverlayPlugin.tsx
+++ src/leaderboard/LeaderboardOverlayPlugin.tsx
@@ -7,15 +7,15 @@ import { IPluginComponentProps } from "@vrkit-platform/plugin-sdk"
 import React from "react"
 import clsx from "clsx"
 import { styled } from "@mui/material/styles"
-import {
-  alpha, createClassNames, FillWidth, FlexAuto, FlexRowCenter, hasCls, rem
-} from "@vrkit-platform/shared-ui"
+// import {
+//   alpha, createClassNames, FillWidth, FlexAuto, FlexRowCenter, hasCls, rem
+// } from "@vrkit-platform/shared-ui"
 
 const log = getLogger(__filename)
 
 const classNamePrefix = "leaderboardPlugin"
 
-const classNames = createClassNames(classNamePrefix, "root")
+// const classNames = createClassNames(classNamePrefix, "root")
 
 const LeaderboardViewRoot = styled(
     Box,
@@ -23,28 +23,28 @@ const LeaderboardViewRoot = styled(
 )(
     ({ theme }) => {
       return {
-        [hasCls(classNames.root)]: {
-          borderRadius: rem(1),
-          backgroundColor: alpha(theme.palette.grey.A700, 0.9),
-          color: theme.palette.getContrastText(theme.palette.grey.A700), ...FillWidth, ...FlexRowCenter,
-          "& > time": {
-            ...FlexAuto
-          }
-        }
+        // [hasCls(classNames.root)]: {
+        //   borderRadius: rem(1),
+        //   backgroundColor: alpha(theme.palette.grey.A700, 0.9),
+        //   color: theme.palette.getContrastText(theme.palette.grey.A700), ...FillWidth, ...FlexRowCenter,
+        //   "& > time": {
+        //     ...FlexAuto
+        //   }
+        // }
       }
     })
 
 
 interface LeaderboardViewProps extends BoxProps {
-  showMillis?:boolean
+
 }
 
 function LeaderboardView({
-  showMillis = false, className, ...other
+  className, ...other
 }:LeaderboardViewProps) {
   return (
       <LeaderboardViewRoot
-          className={clsx(classNames.root, className)} {...other}>
+          className={clsx(className)} {...other}>
         Leaderboard will be here
       </LeaderboardViewRoot>
   )
@@ -54,7 +54,7 @@ function LeaderboardOverlayPlugin(props:IPluginComponentProps) {
   const { client, width, height } = props
   
   return (
-      <LeaderboardView showMillis={false}/>
+      <LeaderboardView />
   )
 }
 
